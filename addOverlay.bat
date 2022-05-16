f61d777bd5aae4bb6bef174d0ccac93cf0524169
Vanderlei Chagas <vanderleichagas42@gmail.com>
2022-05-16 17:54:54 -0300

    feat: Select random background overlay

--- addOverlay.bat
+++ addOverlay.bat
@@ -1,7 +1,7 @@
 
 del "content\output\output.mp4"
 
-Set /a num=%random% %% 3 + 1
+Set /a num=%random% %% 2 + 1
 echo %num%
-ffmpeg  -i overlay/Background-%num%.mp4 -i content/output/slideShow.mp4 -i overlay/Music.mp3 -threads 2  -filter_complex  "overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2; amovie=overlay/Music.mp3:loop=999, volume=0.1[s];[1][s]amix=duration=shortest"  -c:a aac -b:a 192k -c:v libx264 -shortest content/output/output.mp4
+ffmpeg   -i overlay/Background-%num%.mp4 -i content/output/slideShow.mp4 -i overlay/Music.mp3 -threads 2  -filter_complex  "overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2; amovie=overlay/Music.mp3:loop=999, volume=0.1[s];[1][s]amix=duration=shortest"  -c:a aac -b:a 192k -c:v libx264 -shortest content/output/output.mp4
 
