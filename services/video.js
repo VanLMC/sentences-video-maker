77062c82ec2e8b6aaa6a276fd18569a8a06d9956
Vanderlei Chagas <vanderleichagas42@gmail.com>
2022-05-15 22:23:22 -0300

    feat: render video

--- services/video.js
+++ services/video.js
@@ -1,14 +1,12 @@
 
-// const videoShow = require('videoshow')
 const shell = require('shelljs')
-// const state = require('./state.js')
+
 async function videoService() {
-    // const content = state.load()
 
     createSlideShow();
 
     async function createSlideShow() {
-        shell.exec('renderVideos.sh')
+        shell.exec('renderVideos.bat')
     }
 }
 
