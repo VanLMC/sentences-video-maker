bf10b48de7b0741b5f36142478f5019078210773
Vanderlei Chagas <vanderleichagas42@gmail.com>
2022-05-18 10:49:56 -0300

    wip: add zombie

--- services/text.js
+++ services/text.js
@@ -1,6 +1,7 @@
 
 import axios from 'axios'
 import cheerio from 'cheerio'
+import Browser from 'zombie';
 const url = 'https://www.pensador.com/';
 import state from './state.js'
 
@@ -8,8 +9,9 @@ async function textService() {
 
     const content = state.load()
 
-    await getQuotes(content)
-    await addIntroAndOutro(content)
+    // await getQuotes(content)
+    await getQuotesZombie(content)
+    // await addIntroAndOutro(content)
     
     async function getQuotes(content){
         const parsedSearchTerm = content.searchTerm.replace(/\s/g, '_')
@@ -34,6 +36,29 @@ async function textService() {
         state.save(content)
     }
 
+    async function getQuotesZombie(content){
+      
+    // https://stackoverflow.com/questions/33954232/how-can-i-do-a-loop-using-promises-with-nodejs-and-zombie-js
+    
+        let browser = new Browser();
+        console.log(content.searchTerm);
+        const parsedSearchTerm = content.searchTerm.replace(/\s/g, '_')
+        const mountedUrl = `${url}${parsedSearchTerm}`
+        browser.visit(mountedUrl).then(() => {
+            console.log(`Visited ${mountedUrl}..`);
+            var result = browser.queryAll('#phrasesList .thought-card');
+            var cellTextArray = result.map(r => 
+                    // console.log(r.query('.frase').textContent.trim())
+                     console.log(r)
+                )
+            // .filter(text => text && (text || '').length > 3);
+
+            console.log(result);
+        }).catch(error => {
+            console.error(`Error occurred visiting ${mountedUrl}`);
+        });
+    }
+
     async function addIntroAndOutro(content){
         const intro = `${content.quotes.length} frases de ${content.searchTerm}` 
         const outro = `Se você gostou do vídeo, deixe um joinha. Inscreva-se e ative o sininho.` 
@@ -41,7 +66,6 @@ async function textService() {
         content.quotes.push({text: outro, author: ''})
         state.save(content)
     }
-
 
     
 }
