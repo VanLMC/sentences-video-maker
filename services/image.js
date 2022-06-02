ceea2e44761253908e8d31a5acc1b217c1a9532d
Vanderlei Chagas <vanderleichagas42@gmail.com>
2022-06-01 22:46:08 -0300

    add more background images,  format text and image service

--- services/image.js
+++ services/image.js
@@ -1,104 +1,77 @@
-import gm from "gm"
-import path from 'path'
-import state from './state.js'
-import fs from 'fs';
-import canvas from 'canvas'
+import gm from "gm";
+import path from "path";
+import state from "./state.js";
+import fs from "fs";
+import canvas from "canvas";
 
-
-const im = gm.subClass({ imageMagick: true })
+const im = gm.subClass({ imageMagick: true });
 const __dirname = path.resolve();
 async function imagesService() {
-    const content = state.load()
-    const quotesLength = content.quotes.length;
-    const randomNumber = Math.round(Math.random() * (3 - 1) + 1);
-    await createAllSentenceImages(content)
-    await createThumbNail(content);
-    await createVideoBackground();
+  const content = state.load();
+  const quotesLength = content.quotes.length;
+  const randomNumber = Math.round(Math.random() * (3 - 1) + 1);
+  await createAllSentenceImages(content);
+  await createThumbNail(content);
+  await createVideoBackground();
 
-    async function createAllSentenceImages(content){
-        for (let quoteIndex = 0; quoteIndex < content.quotes.length; quoteIndex++) {
-            await createSentenceImage(quoteIndex, content.quotes[quoteIndex])
-          }
-    } 
-
-    async function createSentenceImage(quoteIndex, quote) {
-        return new Promise((resolve, reject) => {
-          const outputFile = path.join(__dirname, 'content', 'images', `${quoteIndex}-sentence.png`)
-    
-          const text = quoteIndex === 0 || quoteIndex + 1 === quotesLength ? quote.text : `"${quote.text}"`
-          const author = quote.author ? ' - ' + quote.author: ''
-
-          const parsedSentence = `${text}\n${author}`
-          im()
-            .out('-size','800x720')
-            .out('-gravity', 'center')
-            .out('-background', 'black')
-            .out('-fill', 'white')
-            .out('-kerning', '-1')
-            .font('Arial', 30) 
-            .in('-weight', '900')
-            .out(`caption:${parsedSentence}`)
-            .write(outputFile, (error) => {
-              if (error) {
-                return reject(error)
-              }
-    
-              console.log(`Sentence created: ${outputFile}`)
-              resolve()
-            })
-        })
+  async function createAllSentenceImages(content) {
+    for (let quoteIndex = 0; quoteIndex < content.quotes.length; quoteIndex++) {
+      await createSentenceImage(quoteIndex, content.quotes[quoteIndex]);
     }
-
-    async function createThumbNail(content) {
-      return new Promise((resolve, reject) => {
-
-
-        const image = path.join(__dirname, 'overlay', `old-man-${randomNumber}.jpg`)
-        const outputFile = path.join(__dirname, 'content', 'output', 'thumbnail.png')
-
-        const longText = content.quotes[10].text
-        const text  = longText.slice(0, 20) + '...'
-
-        const width = 1280;
-        const height = 720;
-        const imagePosition = {
-          w: 440,
-          h: 620,
-          x: 0,
-          y: 5,
-        };
-
-        const c = canvas.createCanvas(width, height);
-        const context = c.getContext("2d");
-        
-        context.fillStyle = "#000";
-        context.fillRect(0, 0, width, height);
-        
-        context.font = 'Arial';
-        context.textAlign = "center";
-        context.fillStyle = "#fff";
-
-        context.font = "50pt 'PT Sans'";
-        context.fillText(text, 800, 300);
-      
-        canvas.loadImage(image).then((image) => {
-          const { w, h, x, y } = imagePosition;
-          context.drawImage(image, x, y, w, h);
-        
-          const buffer = c.toBuffer("image/png");
-          fs.writeFileSync(outputFile, buffer);
-        });
-
-
-        resolve();
-      })
   }
 
-  async function createVideoBackground(content) {
+  async function createSentenceImage(quoteIndex, quote) {
     return new Promise((resolve, reject) => {
+      const outputFile = path.join(
+        __dirname,
+        "content",
+        "images",
+        `${quoteIndex}-sentence.png`
+      );
 
-      const image = path.join(__dirname, 'overlay', `old-man-${randomNumber}.jpg`)
-      const outputFile = path.join(__dirname, 'overlay', 'background-image.png')
+      const text =
+        quoteIndex === 0 || quoteIndex + 1 === quotesLength
+          ? quote.text
+          : `"${quote.text}"`;
+      const author = quote.author ? " - " + quote.author : "";
+
+      const parsedSentence = `${text}\n${author}`;
+      im()
+        .out("-size", "800x720")
+        .out("-gravity", "center")
+        .out("-background", "black")
+        .out("-fill", "white")
+        .out("-kerning", "-1")
+        .font("Arial", 30)
+        .in("-weight", "900")
+        .out(`caption:${parsedSentence}`)
+        .write(outputFile, (error) => {
+          if (error) {
+            return reject(error);
+          }
+
+          console.log(`Sentence created: ${outputFile}`);
+          resolve();
+        });
+    });
+  }
+
+  async function createThumbNail(content) {
+    return new Promise((resolve, reject) => {
+      const image = path.join(
+        __dirname,
+        "overlay",
+        `old-man-${randomNumber}.jpg`
+      );
+      const outputFile = path.join(
+        __dirname,
+        "content",
+        "output",
+        "thumbnail.png"
+      );
+
+      const longText = content.quotes[10].text;
+      const text = longText.slice(0, 20) + "...";
 
       const width = 1280;
       const height = 720;
@@ -111,26 +84,68 @@ async function imagesService() {
 
       const c = canvas.createCanvas(width, height);
       const context = c.getContext("2d");
-      
+
       context.fillStyle = "#000";
       context.fillRect(0, 0, width, height);
-    
+
+      context.font = "Arial";
+      context.textAlign = "center";
+      context.fillStyle = "#fff";
+
+      context.font = "50pt 'PT Sans'";
+      context.fillText(text, 800, 300);
+
       canvas.loadImage(image).then((image) => {
         const { w, h, x, y } = imagePosition;
         context.drawImage(image, x, y, w, h);
-      
+
         const buffer = c.toBuffer("image/png");
         fs.writeFileSync(outputFile, buffer);
       });
 
-
       resolve();
-    })
+    });
   }
 
+  async function createVideoBackground(content) {
+    return new Promise((resolve, reject) => {
+      const image = path.join(
+        __dirname,
+        "overlay",
+        `old-man-${randomNumber}.jpg`
+      );
+      const outputFile = path.join(
+        __dirname,
+        "overlay",
+        "background-image.png"
+      );
 
+      const width = 1280;
+      const height = 720;
+      const imagePosition = {
+        w: 440,
+        h: 620,
+        x: 0,
+        y: 5,
+      };
 
+      const c = canvas.createCanvas(width, height);
+      const context = c.getContext("2d");
+
+      context.fillStyle = "#000";
+      context.fillRect(0, 0, width, height);
+
+      canvas.loadImage(image).then((image) => {
+        const { w, h, x, y } = imagePosition;
+        context.drawImage(image, x, y, w, h);
+
+        const buffer = c.toBuffer("image/png");
+        fs.writeFileSync(outputFile, buffer);
+      });
+
+      resolve();
+    });
+  }
 }
-
 
 export default imagesService;
--- services/text.js
+++ services/text.js
@@ -1,57 +1,48 @@
-
-import axios from 'axios'
-import cheerio from 'cheerio'
-const url = 'https://www.pensador.com/';
-import state from './state.js'
+import axios from "axios";
+import cheerio from "cheerio";
+const url = "https://www.pensador.com/";
+import state from "./state.js";
 
 async function textService() {
+  const content = state.load();
 
-    const content = state.load()
+  await getQuotes(content);
+  await getQuotes(content, 2);
+  await addIntroAndOutro(content);
 
-     await getQuotes(content)
-     await getQuotes(content, 2)
-     await addIntroAndOutro(content)
-    
-    async function getQuotes(content, page = ''){
-        const parsedSearchTerm = content.searchTerm.toLowerCase().replace(/\s/g, '_')
-        const {data} = await axios.get(`${url}${parsedSearchTerm}/${page}/`);
+  async function getQuotes(content, page = "") {
+    const parsedSearchTerm = content.searchTerm
+      .toLowerCase()
+      .replace(/\s/g, "_");
+    const { data } = await axios.get(`${url}${parsedSearchTerm}/${page}/`);
 
-        const quotes = page !== '' ? [...content.quotes] : [];
-        const $ = cheerio.load(data);
+    const quotes = page !== "" ? [...content.quotes] : [];
+    const $ = cheerio.load(data);
 
+    $("#phrasesList .thought-card").each((i, element) => {
+      const text = $(element).find(".frase").text().replace("\n", "");
+      const autor = $(element).find(".autor>a").text();
 
-        $('#phrasesList .thought-card').each((i, element) => {
-            const text = $(element).find('.frase').text().replace('\n', '');
-            const autor = $(element).find('.autor>a').text();
+      if (text.length > 1000) return;
+      quotes.push({ text: text, author: autor });
+    });
 
-            if(text.length > 1000) return
-            quotes.push({text: text, author: autor})
-        });
+    const sortedQuotes = quotes.sort(function (a, b) {
+      return a.text.length - b.text.length;
+    });
 
+    content.quotes = sortedQuotes;
 
-        const sortedQuotes = quotes.sort(function(a, b){
-            return a.text.length - b.text.length;
-          });
+    state.save(content);
+  }
 
-
-        content.quotes =  sortedQuotes
-
-        state.save(content)
-    }
-
-
-    async function addIntroAndOutro(content){
-        const intro = `${content.quotes.length} frases de ${content.searchTerm}` 
-        const outro = `Comente sua frase favorita! Se você gostou do vídeo, deixe um joinha. Inscreva-se e ative o sininho.` 
-        content.quotes.unshift({text: intro, author: ''})
-        content.quotes.push({text: outro, author: ''})
-        state.save(content)
-    }
-
-    
+  async function addIntroAndOutro(content) {
+    const intro = `${content.quotes.length} frases de ${content.searchTerm}`;
+    const outro = `Comente sua frase favorita! Se você gostou do vídeo, deixe um joinha. Inscreva-se e ative o sininho.`;
+    content.quotes.unshift({ text: intro, author: "" });
+    content.quotes.push({ text: outro, author: "" });
+    state.save(content);
+  }
 }
-
-
-
 
 export default textService;
